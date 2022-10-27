61b2a65614e8a8166f1bbc063b7f224afd5e7aac
gernotroth <gernotroth@hotmail.com>
2022-10-27 17:13:59 +0200

    add test

--- basics.test/UnitTest1.cs
+++ basics.test/UnitTest1.cs
@@ -11,5 +11,13 @@ namespace basics
             var sut = new Worker();
             Assert.IsTrue(sut.DoStuff());
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var sut = new Worker();
+            var input = 4;
+            Assert.AreEqual(input + 1, sut.AddOne(input));
+        }
     }
 }
